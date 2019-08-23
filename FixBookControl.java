9963a2729e09e736f8509144861f51fe101eb4cf
ksulth01 <53158790+ksulth01@users.noreply.github.com>
2019-08-23 22:18:58 +1000

    AUTHOR SUL
    author sulthan -- SUL

--- FixBookControl.java
+++ FixBookControl.java
@@ -1,72 +1,79 @@
 public class FixBookControl {
 	
+	//author by Sulthan --SUL
 	private FixBookUI UI;
-	private enum CONTROL_STATE { INITIALISED, READY, FIXING };
-	private CONTROL_STATE StAtE;
+	private enum ControlState { INITIALISED, READY, FIXING }; //CONTROL_STATE to ControlState -- SUL
+	private ControlState state; //StAtE to state -- SUL
 	
 	private library LIB;
-	private book Cur_Book;
+	private book curBook; //removed underscore and capatilized first letter -- SUL
 
 
-	public FixBookControl() {
+	public FixBookControl() 
+	{
 		this.LIB = LIB.INSTANCE();
-		StAtE = CONTROL_STATE.INITIALISED;
+		state = ControlState.INITIALISED; //StAtE to state -- SUL
 	}
 	
 	
-	public void Set_Ui(FixBookUI ui) {
-		if (!StAtE.equals(CONTROL_STATE.INITIALISED)) {
+	public void set_Ui(FixBookUI ui) //Set to set -- SUL
+	{
+		if (!state.equals(ControlState.INITIALISED)) //StAtE to state -- SUL
+		{
 			throw new RuntimeException("FixBookControl: cannot call setUI except in INITIALISED state");
 		}	
 		this.UI = ui;
-		ui.Set_State(FixBookUI.UI_STATE.READY);
-		StAtE = CONTROL_STATE.READY;		
+		ui.set_State(FixBookUI.UI_STATE.READY); //Set to set -- SUL
+		state = ControlState.READY;		//StAtE to state -- SUL
 	}
 
 
-	public void Book_scanned(int bookId) {
-		if (!StAtE.equals(CONTROL_STATE.READY)) {
+	public void bookScanned(int bookId) //Book_Scanned to bookScanned --SUL
+	{
+		if (!state.equals(ControlState.READY)) //StAtE to state -- SUL
+		{
 			throw new RuntimeException("FixBookControl: cannot call bookScanned except in READY state");
 		}	
-		Cur_Book = LIB.Book(bookId);
+		curBook = LIB.Book(bookId); //removed underscore and capatilized first letter -- SUL
 		
-		if (Cur_Book == null) {
+		if (curBook == null) //removed underscore and capatilized first letter -- SUL
+		{
 			UI.display("Invalid bookId");
 			return;
 		}
-		if (!Cur_Book.IS_Damaged()) {
+		if (!curBook.IS_Damaged()) //removed underscore and capatilized first letter -- SUL
+		{
 			UI.display("Book has not been damaged");
 			return;
 		}
 		UI.display(Cur_Book.toString());
 		UI.Set_State(FixBookUI.UI_STATE.FIXING);
-		StAtE = CONTROL_STATE.FIXING;		
+		state = ControlState.FIXING;	//StAtE to state -- SUL	
 	}
 
 
-	public void FIX_Book(boolean MUST_fix) {
-		if (!StAtE.equals(CONTROL_STATE.FIXING)) {
+	public void fixBook(boolean MUST_fix) //FIX_Book to fixBook -- SUL
+	{
+		if (!state.equals(ControlState.FIXING)) //StAtE to state -- SUL
+		{
 			throw new RuntimeException("FixBookControl: cannot call fixBook except in FIXING state");
 		}	
-		if (MUST_fix) {
-			LIB.Repair_BOOK(Cur_Book);
+		if (MUST_fix) 
+		{
+			LIB.Repair_BOOK(curBook); //removed underscore and capatilized first letter -- SUL
 		}
-		Cur_Book = null;
+		curBook = null; //removed underscore and capatilized first letter -- SUL
 		UI.Set_State(FixBookUI.UI_STATE.READY);
-		StAtE = CONTROL_STATE.READY;		
+		state = ControlState.READY;	//StAtE to state -- SUL	
 	}
 
 	
-	public void SCannING_COMplete() {
-		if (!StAtE.equals(CONTROL_STATE.READY)) {
+	public void scanningComplete()   //changed irregular capatilized words into order -SUL
+	{
+		if (!state.equals(ControlState.READY)) //StAtE to state -- SUL
+		{
 			throw new RuntimeException("FixBookControl: cannot call scanningComplete except in READY state");
 		}	
 		UI.Set_State(FixBookUI.UI_STATE.COMPLETED);		
 	}
-
-
-
-
-
-
 }
--- FixBookUI.java
+++ FixBookUI.java
@@ -1,44 +1,52 @@
 import java.util.Scanner;
 
+//author by Sulthan --SUL
+public class FixBookUI 
+{
 
-public class FixBookUI {
+	public static enum UiState { INITIALISED, READY, FIXING, COMPLETED }; //UI_STATE to UiState -- SUL
 
-	public static enum UI_STATE { INITIALISED, READY, FIXING, COMPLETED };
-
-	private FixBookControl CoNtRoL;
+	private FixBookControl control; //CoNtRoL to control -- SUL
 	private Scanner input;
-	private UI_STATE StAtE;
+	private UiState state; //StAtE to state UI_STATE to UiState -- SUL
 
 	
-	public FixBookUI(FixBookControl control) {
-		this.CoNtRoL = control;
+	public FixBookUI(FixBookControl control) 
+	{
+		this.control = control; //CoNtRoL to control -- SUL
 		input = new Scanner(System.in);
-		StAtE = UI_STATE.INITIALISED;
+		state = UiState.INITIALISED; //StAtE to state UI_STATE to UiState -- SUL
 		control.Set_Ui(this);
 	}
 
 
-	public void Set_State(UI_STATE state) {
-		this.StAtE = state;
+	public void Set_State(UiState state) 
+	{
+		this.state = state; //StAtE to state -- SUL
 	}
 
 	
-	public void RuN() {
+	public void Run() //RuN to Run -- SUL
+	{
 		output("Fix Book Use Case UI\n");
 		
-		while (true) {
+		while (true) 
+			{
 			
-			switch (StAtE) {
+			switch (state) 
+			{ //StAtE to state -- SUL
 			
 			case READY:
 				String Book_STR = input("Scan Book (<enter> completes): ");
 				if (Book_STR.length() == 0) {
-					CoNtRoL.SCannING_COMplete();
+					control.Scanning_complete(); //CoNtRoL to control SCanning_Complete to Scanning_complete -- SUL
 				}
-				else {
-					try {
+				else 
+					{
+					try 
+					{
 						int Book_ID = Integer.valueOf(Book_STR).intValue();
-						CoNtRoL.Book_scanned(Book_ID);
+						control.Book_scanned(Book_ID); //CoNtRoL to control -- SUL
 					}
 					catch (NumberFormatException e) {
 						output("Invalid bookId");
@@ -47,12 +55,13 @@ public class FixBookUI {
 				break;	
 				
 			case FIXING:
-				String AnS = input("Fix Book? (Y/N) : ");
-				boolean FiX = false;
-				if (AnS.toUpperCase().equals("Y")) {
-					FiX = true;
+				String ans = input("Fix Book? (Y/N) : "); //AnS to ans -- SUL
+				boolean Fix = false;
+				if (ans.toUpperCase().equals("Y")) 
+				{ //AnS to ans -- SUL
+					Fix = true; //FiX to Fix -- SUL
 				}
-				CoNtRoL.FIX_Book(FiX);
+				control.FIX_Book(Fix); //CoNtRoL to control -- SUL
 				break;
 								
 			case COMPLETED:
@@ -61,7 +70,7 @@ public class FixBookUI {
 			
 			default:
 				output("Unhandled state");
-				throw new RuntimeException("FixBookUI : unhandled state :" + StAtE);			
+				throw new RuntimeException("FixBookUI : unhandled state :" + state);	//StAtE to state -- SUL		
 			
 			}		
 		}
@@ -69,18 +78,21 @@ public class FixBookUI {
 	}
 
 	
-	private String input(String prompt) {
+	private String input(String prompt) 
+	{
 		System.out.print(prompt);
 		return input.nextLine();
 	}	
 		
 		
-	private void output(Object object) {
+	private void output(Object object) 
+	{
 		System.out.println(object);
 	}
 	
 
-	public void display(Object object) {
+	public void display(Object object) 
+	{
 		output(object);
 	}
 	
